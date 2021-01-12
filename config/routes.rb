737c6fe77f3ac4c5b3eb2d3837f4bad2b93b105f
Adam Ahrens <adam.aahrens@gmail.com>
2021-01-12 11:43:15 -0600

    Adding a Users index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root 'pages#landing'
 
   devise_for :users
+
   resources :tracks
+  resources :users , only: [:index]
   
   get 'privacy', to: 'pages#privacy'
   get 'about', to: 'pages#about'
-
   post 'search', to: 'tracks#index'
 end
